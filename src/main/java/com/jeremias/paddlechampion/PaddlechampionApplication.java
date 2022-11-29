f2b509c48fa11871fa17470fa346f4fc4f40b687
JeremiasOviedo <JeremiasOviedo.Dev@gmail.com>
2022-11-28 21:59:26 -0300

    PC-1: Started and configured the project

---
+++ src/main/java/com/jeremias/paddlechampion/PaddlechampionApplication.java
@@ -0,0 +1,13 @@
+package com.jeremias.paddlechampion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaddlechampionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaddlechampionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeremias/paddlechampion/PaddlechampionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeremias.paddlechampion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaddlechampionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
